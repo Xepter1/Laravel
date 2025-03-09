27613a41d33d16d84436a5c782cb8ec5b048e8b6
Xepter1 <fraunhoferpatrick@gmil.com>
2025-03-09 16:57:57 +0100

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja Network</title>
+</head>
+<body>
+    <h1>Welcome to Network</h1>
+    <p>click the button below</p>
+
+    <a href="" class="button">
+        Find Ninja
+    </a>
+</body>
+</html>
