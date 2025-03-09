9c44c428a03f79b7b9293ac258b120c62c2926c3
Xepter1 <fraunhoferpatrick@gmil.com>
2025-03-09 19:48:26 +0100

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +9,14 @@ Route::get('/', function () {
 
 Route::get('/ninjas',function(){
     return view('ninjas.index');
+});
+
+// Neue Route zum Testen der Datenbankverbindung
+Route::get('/dbtest', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Verbindung zur Datenbank erfolgreich!';
+    } catch (\Exception $e) {
+        return 'Keine Verbindung zur Datenbank: ' . $e->getMessage();
+    }
 });
